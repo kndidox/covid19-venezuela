cb316c2f95a9bf472d2d64992ba41afb5c20ba29
Candido <kndidox@gmail.com>
2020-03-19 10:55:20 -0400

    update march 18 2020

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 //Total Cases
-let x_dates = ['13-03-2020','14-03-2020','15-03-2020','16-03-2020', '17-03-2020']
+let x_dates = [
+  '13-03-2020','14-03-2020','15-03-2020','16-03-2020', '17-03-2020','18-03-2020']
 
 var trace = {
   x: x_dates,
-  y: [2,10,17,33,36],
+  y: [2,10,17,33,36,36],
   mode: 'lines+markers',
   type: 'scatter'
 };
@@ -13,7 +14,7 @@ Plotly.newPlot('graph01', data);
 //Total Cases by day
 var trace_02 = {
   x: x_dates,
-  y: [2,8,7,16,3],
+  y: [2,8,7,16,3,0],
   mode: 'lines+markers',
   type: 'scatter'
 };
@@ -23,63 +24,63 @@ Plotly.newPlot('graph02', data_02);
 //Total Cases by State
 var state_apure = {
   x: x_dates,
-  y: [0,1,1,1,1],
+  y: [0,1,1,1,1,1],
   mode: 'lines+markers',
   type: 'scatter',
   name: 'Apure'
 };
 var state_anzoategui = {
   x: x_dates,
-  y: [0,0,0,2,2],
+  y: [0,0,0,2,2,2],
   mode: 'lines+markers',
   type: 'scatter',
   name: 'Anzoategui'
 };
 var state_aragua = {
   x: x_dates,
-  y: [0,1,1,2,2],
+  y: [0,1,1,2,2,2],
   mode: 'lines+markers',
   type: 'scatter',
   name: 'Aragua'
 };
 var state_caracas = {
   x: x_dates,
-  y: [0,0,0,5,5],
+  y: [0,0,0,5,5,5],
   mode: 'lines+markers',
   type: 'scatter',
   name: 'Caracas'
 };
 var state_cojedes = {
   x: x_dates,
-  y: [0,1,1,1,1],
+  y: [0,1,1,1,1,1],
   mode: 'lines+markers',
   type: 'scatter',
   name: 'Cojedes'
 };
 var state_guaira = {
   x: x_dates,
-  y: [0,0,0,5,5],
+  y: [0,0,0,5,5,5],
   mode: 'lines+markers',
   type: 'scatter',
   name: 'La Guaira'
 };
 var state_merida = {
   x: x_dates,
-  y: [0,0,0,1,1],
+  y: [0,0,0,1,1,1],
   mode: 'lines+markers',
   type: 'scatter',
   name: 'Merida'
 };
 var state_miranda = {
   x: x_dates,
-  y: [2,6,11,13,19],
+  y: [2,6,11,13,19,19],
   mode: 'lines+markers',
   type: 'scatter',
   name: 'Miranda'
 };
 var state_unknown = {
   x: x_dates,
-  y: [0,0,3,0,0],
+  y: [0,0,3,0,0,0],
   mode: 'lines+markers',
   type: 'scatter',
   name: 'Unknow'
